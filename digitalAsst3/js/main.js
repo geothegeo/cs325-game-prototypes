ce912677f135c8d2141e825780043d77e00e78e7
George Tang <georgethewebmaster@gmail.com>
2021-03-13 22:17:26 -0600

    digitalAsst4 Revision and digitalAsst5 Addition

--- digitalAsst3/js/main.js
+++ digitalAsst3/js/main.js
@@ -1,101 +1,90 @@
 import "./phaser.js";
 
-var game = new Phaser.Game({
-    type: Phaser.AUTO,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    mode: Phaser.Scale.FIT,
-    parent: 'game',
+var config = {
+    type: Phaser.CANVAS,
+    // autoCenter: Phaser.Scale.CENTER_BOTH,
+    // mode: Phaser.Scale.FIT,
     width: 800,
     height: 960,
-    scene: MyScene
-});
-
-var rotationSpeed = 3;
-var maxAngleDifference = 10;
-
-var MyScene = new Phaser.Class({
-
-    Extends: Phaser.Scene,
-
-    initialize:
-    
-    function MyScene () {
-
-        Phaser.Scene.call(this, { key: 'MyScene', active: true });
-
-        this.frames;
-    },
-
-    preload: function() {
-        // Load an image and call it 'logo'.
-        this.load.image("ball", "assets/ball.png");
-        this.load.image("bar", "assets/bar.png");
-        this.load.image("ring", "assets/ring.png");
-    },
-    
-    create() {
-        game.stage.backgroundColor = "#4488AA";
-
-        var ring = game.add.sprite(game.width / 2, game.height / 2, "ring");
-        ring.anchor.set(0.5);
-        ring.alpha = 0.5;
-
-        this.ball = game.add.sprite(0, 0, "ball");
-        this.ball.anchor.set(0.5);
-        this.ball.ballAngle = -90;
-        this.placeBall();
-
-        this.bar = game.add.sprite(game.width / 2, game.height / 2, "bar");
-        this.bar.anchor.set(0, 0.5);
-        this.bar.angle = -90;
-        this.bar.crossingBall = false;
-        this.bar.rotationDirection = 0;
-        game.input.onDown.add(this.startMoving, this);
-
-    },
-    
-    update() {
-        this.bar.angle += rotationSpeed * this.bar.rotationDirection;
-        var angleDifference = Math.abs(this.ball.ballAngle - this.bar.angle);
-        if (angleDifference < maxAngleDifference && !this.bar.crossingBall) {
-            this.bar.crossingBall = true;
-        }
-        if (angleDifference > maxAngleDifference && this.bar.crossingBall) {
-            this.fail();
-        }
-    },
-
-    placeBall: function() {
-        do {
-          var newAngle = game.rnd.angle();
-        } while (angleDifference(newAngle, this.ball.ballAngle) < 40)
-        this.ball.ballAngle = newAngle;
-        this.ball.x = game.width / 2 + 175 * Math.cos(Phaser.Math.degToRad(this.ball.ballAngle));
-        this.ball.y = game.height / 2 + 175 * Math.sin(Phaser.Math.degToRad(this.ball.ballAngle));
-      },
-
-    startMoving: function() {
-        game.input.onDown.remove(this.startMoving, this);
-        game.input.onDown.add(this.changeDirection, this);
-        this.bar.rotationDirection = 1;
-    },
-
-    changeDirection: function() {
-        var angleDifference = Math.abs(this.ball.ballAngle - this.bar.angle);
-        if (angleDifference > maxAngleDifference) {
-            this.fail();
-        } else {
-            this.bar.crossingBall = false;
-            this.bar.rotationDirection *= -1;
-            this.placeBall();
-        }
-    },
-
-    fail: function () {
-        this.bar.rotationDirection = 0; 
-        this.bar.tint = 0xff0000;
+    backgroundColor: '#4488AA',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
-});
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('ball', 'assets/ball.png');
+    this.load.image('bar', 'assets/bar.png');
+    this.load.image('ring', 'assets/ring.png');
+}
+
+var ring;
+var ball;
+var bar;
+
+function create() {
+    ring = this.add.sprite(400, 480, 'ring');
+    // ball.anchor.set(0.5);
+    ring.alpha = 0.5;
+
+    ball = this.add.sprite(0, 0, "ball");
+    // ball.anchor.set(0.5);
+    ball.ballAngle = -90;
+    placeBall();
+
+    bar = this.add.sprite(400, 480, "bar");
+    // bar.anchor.set(0, 0.5);
+    bar.angle = -90;
+    bar.crossingBall = false;
+    bar.rotationDirection = 0;
+    this.input.onDown.add(startMoving(), this);
+}
+
+function placeBall() {
+    do {
+        var newAngle = Math.random(360) - 180;
+    } while (angleDifference(newAngle, ball.ballAngle) < 40)
+    ball.ballAngle = newAngle;
+    ball.x = game.width / 2 + 175 * Math.cos(ball.ballAngle * Math.PI/180);
+    ball.y = game.height / 2 + 175 * Math.sin(ball.ballAngle * Math.PI/180);
+    }
+
+function startMoving() {
+    this.input.onDown.remove(startMoving(), this);
+    this.input.onDown.add(changeDirection(), this);
+    bar.rotationDirection = 1;
+}
+
+function changeDirection() {
+    var angleDifference = Math.abs(ball.ballAngle - bar.angle);
+    if (angleDifference > maxAngleDifference) {
+        fail();
+    } else {
+        bar.crossingBall = false;
+        bar.rotationDirection *= -1;
+        placeBall();
+    }
+}
+
+function fail() {
+    bar.rotationDirection = 0; 
+    bar.tint = 0xff0000;
+}
+
+function update() {
+    bar.angle += rotationSpeed * bar.rotationDirection;
+    var angleDifference = Math.abs(ball.ballAngle - bar.angle);
+    if (angleDifference < maxAngleDifference && !bar.crossingBall) {
+        bar.crossingBall = true;
+    }
+    if (angleDifference > maxAngleDifference && bar.crossingBall) {
+        fail();
+    }
+}
 
 function angleDifference(a1, a2) {
     return Math.abs((a1 + 180 - a2) % 360 - 180);
--- digitalAsst4/js/main.js
+++ digitalAsst4/js/main.js
@@ -1,102 +1,216 @@
 import "./phaser.js";
 
-var game = new Phaser.Game({
+var config = {
     type: Phaser.AUTO,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    mode: Phaser.Scale.FIT,
-    parent: 'game',
     width: 800,
-    height: 960,
-    scene: MyScene
-});
-
-var rotationSpeed = 3;
-var maxAngleDifference = 10;
-
-var MyScene = new Phaser.Class({
-
-    Extends: Phaser.Scene,
-
-    initialize:
-    
-    function MyScene () {
-
-        Phaser.Scene.call(this, { key: 'MyScene', active: true });
-
-        this.frames;
-    },
-
-    preload: function() {
-        // Load an image and call it 'logo'.
-        this.load.image("ball", "assets/ball.png");
-        this.load.image("bar", "assets/bar.png");
-        this.load.image("ring", "assets/ring.png");
-    },
-    
-    create() {
-        game.stage.backgroundColor = "#4488AA";
-
-        var ring = game.add.sprite(game.width / 2, game.height / 2, "ring");
-        ring.anchor.set(0.5);
-        ring.alpha = 0.5;
-
-        this.ball = game.add.sprite(0, 0, "ball");
-        this.ball.anchor.set(0.5);
-        this.ball.ballAngle = -90;
-        this.placeBall();
-
-        this.bar = game.add.sprite(game.width / 2, game.height / 2, "bar");
-        this.bar.anchor.set(0, 0.5);
-        this.bar.angle = -90;
-        this.bar.crossingBall = false;
-        this.bar.rotationDirection = 0;
-        game.input.onDown.add(this.startMoving, this);
-
-    },
-    
-    update() {
-        this.bar.angle += rotationSpeed * this.bar.rotationDirection;
-        var angleDifference = Math.abs(this.ball.ballAngle - this.bar.angle);
-        if (angleDifference < maxAngleDifference && !this.bar.crossingBall) {
-            this.bar.crossingBall = true;
-        }
-        if (angleDifference > maxAngleDifference && this.bar.crossingBall) {
-            this.fail();
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
         }
     },
-
-    placeBall: function() {
-        do {
-          var newAngle = game.rnd.angle();
-        } while (angleDifference(newAngle, this.ball.ballAngle) < 40)
-        this.ball.ballAngle = newAngle;
-        this.ball.x = game.width / 2 + 175 * Math.cos(Phaser.Math.degToRad(this.ball.ballAngle));
-        this.ball.y = game.height / 2 + 175 * Math.sin(Phaser.Math.degToRad(this.ball.ballAngle));
-      },
-
-    startMoving: function() {
-        game.input.onDown.remove(this.startMoving, this);
-        game.input.onDown.add(this.changeDirection, this);
-        this.bar.rotationDirection = 1;
-    },
-
-    changeDirection: function() {
-        var angleDifference = Math.abs(this.ball.ballAngle - this.bar.angle);
-        if (angleDifference > maxAngleDifference) {
-            this.fail();
-        } else {
-            this.bar.crossingBall = false;
-            this.bar.rotationDirection *= -1;
-            this.placeBall();
-        }
-    },
-
-    fail: function () {
-        this.bar.rotationDirection = 0; 
-        this.bar.tint = 0xff0000;
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
-});
+};
 
-function angleDifference(a1, a2) {
-    return Math.abs((a1 + 180 - a2) % 360 - 180);
+var player;
+var stars;
+var bombs;
+var platforms;
+var movingPlatform;
+var gameOver = false;
+var time_delay = 45000;
+var timerText;
+var timedEvent;
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', 'assets/room.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('groundS', 'assets/platformS.png');
+    this.load.image('star', 'assets/bearS.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky').setScale(2);
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 605, 'ground').setScale(2).refreshBody();
+    movingPlatform = this.physics.add.image(200, 450, 'groundS');
+    platforms.create(500, 336, 'groundS');
+    platforms.create(200, 252, 'groundS');
+    platforms.create(Phaser.Math.Between(600, 650), 174, 'groundS');
+    
+    movingPlatform.setImmovable(true);
+    movingPlatform.body.allowGravity = false;
+    movingPlatform.setVelocityX(50);
+
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 9,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.75, 0.1));
+        child.setCollideWorldBounds(true);
+        child.setVelocityX(Phaser.Math.Between(-1, 1) * 50);
+        child.allowGravity = false;
+    });
+
+    bombs = this.physics.add.group();
+
+    timerText = this.add.text(16, 16, ' ', { fontSize: '28px', fill: '#2510e2' });
+    timedEvent = this.time.addEvent({ delay: time_delay, callback: killGame, callbackScope: this});
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(bombs, platforms);
+    this.physics.add.collider(player, movingPlatform);
+    this.physics.add.collider(stars, movingPlatform);
+    this.physics.add.collider(bombs, movingPlatform);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+}
+
+function update ()
+{
+    var pointer = this.input.activePointer;
+
+    if (gameOver)
+    {
+        return;
+    }
+
+    timerText.setText('Pick up your toys before time runs out! \nTimer(sec): ' + timedEvent.getRemainingSeconds().toFixed(1));
+
+    if (player.body.touching.down && !pointer.isDown) 
+    {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+    else if (pointer.isDown) 
+    {
+        if (pointer.x <= player.x && pointer.y > player.y)
+        {
+            player.setVelocityX(-160);
+            player.anims.play('left', true);
+        }     
+        else if (pointer.x <= player.x && (pointer.y <= player.y) && player.body.touching.down ) 
+        {
+            player.setVelocityX(-160);
+            player.setVelocityY(-330);
+            player.anims.play('left', true);
+        }
+
+        else if (pointer.x > player.x && pointer.y > player.y)
+        {
+            player.setVelocityX(160);
+            player.anims.play('right', true);
+        }     
+        else if (pointer.x > player.x && (pointer.y <= player.y) && player.body.touching.down ) 
+        {
+            player.setVelocityX(160);
+            player.setVelocityY(-330);
+            player.anims.play('right', true);
+        }
+    } 
+
+    if (movingPlatform.x >= 600)
+    {
+        movingPlatform.setVelocityX(-50);
+    }
+    else if (movingPlatform.x <= 200)
+    {
+        movingPlatform.setVelocityX(50);
+    }
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+            child.enableBody(true, child.x, 0, true, true);
+        });
+
+        if (time_delay >= 23000)
+            time_delay -= 3000;
+        this.time.removeEvent(timedEvent);
+        timedEvent = this.time.addEvent({ delay: time_delay, callback: killGame, callbackScope: this});
+
+        var star = stars.create(Phaser.Math.Between(0, 800), 16, 'star');
+        star.setBounceY(Phaser.Math.FloatBetween(0.75, 1));
+        star.setCollideWorldBounds(true);
+        star.setVelocityX(Phaser.Math.Between(-1, 1) * 50);
+        star.allowGravity = false;
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+    }
+}
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+}
+
+function killGame()
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
 }
