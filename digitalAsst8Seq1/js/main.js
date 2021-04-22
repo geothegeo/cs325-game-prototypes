4bee84a24bd14dd50643274232ee7b6f6e25abbb
George Tang <georgethewebmaster@gmail.com>
2021-04-22 15:03:21 -0400

    Digital Asst Sequence 2 iteration 1 revision 1

--- digitalAsst8Seq1/js/main.js
+++ digitalAsst8Seq1/js/main.js
@@ -1,22 +1,20 @@
 import "./phaser.js";
 
+const { abs, max, min } = Math;
+const { BetweenPoints } = Phaser.Math.Distance;
+
 // var path;
 var curve;
 var points;
 var graphics;
 
-var startX;
-var endX;
-var point;
-var location;
-var px;
-var distance;
-
 var text;
 var ropeTimer = 0;
 var gameStart = false;
-var speed = 1;
+var speed = 1.5;
 var gameOver = false;
+
+const cursor = new Phaser.Math.Vector2();
 
 function create ()
 {
@@ -33,50 +31,24 @@ function create ()
         900, 432
     ];
 
-    startX = points[0];
-    endX = points[points.length - 2];
-
-    console.log(startX, endX);
-
     curve = new Phaser.Curves.Spline(points);
 
     curve.draw(graphics, 64);
 
-    point = curve.getPointAt(0);
-
-    location = this.add.rectangle(point.x, point.y, 16, 16, 0xff00ff, 0.8);
-
     text = this.add.text(32, 32);
 
-    this.input.on('pointermove', pointer => {
-
-        //  getPointAt requires a value between 0 and 1 (start and end of curve)
-        //  We know the start and end x coordinate of the curve, so we can calculate it from that
-
-        let px = pointer.worldX;
-        distance = endX - startX;
-
-        if (px >= startX && px <= endX)
-        {
-            px -= startX;
-
-            curve.getPointAt(px / distance, point);
-
-            location.setPosition(point.x, point.y);
-        }
-
+    this.input.on('pointermove', function(pointer) {
+        cursor.set(pointer.worldX, pointer.worldY);
     });
 }
 
 function update ()
 {
-    var pointer = this.input.activePointer;
-
-    this.input.on('pointerdown', function (pointer) {
+    this.input.on('pointerdown', function(pointer) {
         gameStart = true;
     }, this);
 
-    if(gameStart) {
+    if(gameStart && !gameOver) {
         ropeTimer += 1;
         // text.setText('Seconds passed: ' + ropeTimer.toString());
     
@@ -97,49 +69,116 @@ function update ()
             points.push(Phaser.Math.Between(900, 1000));
             points.push(Phaser.Math.Between(50, 550));
         } 
-    
+
         curve = new Phaser.Curves.Spline(points);
     
         curve.draw(graphics, 64);
 
         // --------------------------------------------------------------------
 
-        if(points[0] <= 0) {
-            startX = 0;
-        } else {
-            startX = points[0];
-        }
-        endX = 800;
-    
-        distance = endX - startX;
-
-        if (px >= startX && px <= endX)
-        {  
-            curve.getPointAt(px / distance, point);
-
-            location.setPosition(point.x, point.y);
-        }
+        graphics.fillStyle(0xFF0000, 0.8).fillCircle(cursor.x, cursor.y, 10);
+  
+        const closest = getClosestR(cursor, curve, 10, 3, BetweenPoints);
+      
+        drawPoint(closest.point, 0x0000FF, 0.8);
 
         text.setText([
             'Seconds passed: ' + ropeTimer.toString(),
-            'Pointer x: ' + pointer.worldX,
-            'Pointer y: ' + pointer.worldY,
-            'Curve x: ' + point.x,
-            'Curve y: ' + point.y,
-            'dist: ' + Phaser.Math.Distance.BetweenPoints(point, pointer),
             'speed: ' + speed,
+            'distance: ' + closest.distance,
             'gameOver: ' + gameOver
         ]);
 
-        if(Phaser.Math.Distance.BetweenPoints(point, pointer) >= 50) {
+        if(closest.distance >= 25) {
             gameOver = true;
+            graphics.clear();
+            return;
         } else {
             gameOver = false;
         }
 
         if(ropeTimer / 1000 == 1) 
             speed += .5;
+    } else {
+        cursor.set(points[0], points[1]);
     }
+}
+
+function drawPoint(p, color, alpha) {
+    graphics
+      .fillStyle(color, alpha)
+      .lineStyle(1, color, alpha)
+      .fillCircle(p.x, p.y, 10)
+      .lineBetween(p.x, p.y, cursor.x, cursor.y);
+  }
+  
+function BetweenPointsX(a, b) {
+    return abs(a.x - b.x);
+}
+
+function BetweenPointsY(a, b) {
+    return abs(a.y - b.y);
+}
+
+function getClosest(target, curve, start, end, divisions, getDistance) {
+    let d = (end - start) / divisions;
+    let m = Infinity;
+    let result = null;
+
+    for (let t = start; t <= end; t += d) {
+        const point = curve.getPoint(t);
+        const distance = getDistance(target, point);
+        const tLow = max(0, t - d);
+        const tHigh = min(1, t + d);
+
+        if (distance < m) {
+            m = distance;
+            result = {
+                d,
+                distance,
+                point,
+                t,
+                tLow,
+                tHigh,
+                tRange: tHigh- tLow,
+                dx: target.x - point.x,
+                dy: target.y - point.y,
+            };
+        }
+    }
+
+    return result;  
+}
+
+function getClosestR(target, curve, divisions, iterations, getDistance) {
+    let result = { tLow: 0, tHigh: 1 };
+
+    while (iterations > 0) {
+        iterations -= 1;
+
+        result = getClosest(
+        target,
+        curve,
+        result.tLow,
+        result.tHigh,
+        divisions,
+        getDistance
+        );
+    }
+
+    return result;
+}
+
+function objToString(o) {
+    return JSON.stringify(o, replace, 2);
+}
+
+function replace(k, v) {
+    if (k && v && v.toFixed) {
+        return Number(v.toFixed(3));
+    }
+
+    return v;
 }
 
 var config = {
